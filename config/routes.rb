e7783e895b5dc688b6f8f69dfc601cfbfe7cc0d2
sekmo <francescojjmari@gmail.com>
2020-01-03 22:56:09 +0100

    Add User model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#show', page: 'welcome'
   get  'pages/:page', to: 'pages#show', as: 'pages'
