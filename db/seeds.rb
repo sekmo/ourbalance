3edbc718442357de5f8b52928c3222bdc3bac9b0
Francesco Mari <francescojjmari@gmail.com>
2020-01-11 00:31:59 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+DEFAULT_CATEGORIES = [
+  { name: "Groceries" },
+  { name: "Coffee and Cakes" },
+  { name: "Dinner out" },
+  { name: "Outing", description: "Beers etc." },
+  { name: "Clothes and accessories" },
+  { name: "Lunch and Brunch" },
+  { name: "Office lunch" },
+  { name: "Health and Fitness" },
+  { name: "Electronics" },
+  { name: "Home improvements", description: "Furniture, accessories" },
+  { name: "Entertainment", description: "Movies, concerts, theatre" },
+  { name: "Beauty", description: "Hairdresser/Barbershop, moisturizer, etc." },
+  { name: "Culture and learning", description: "Books, courses" },
+  { name: "Honey gifts" },
+  { name: "Friends gifts" },
+  { name: "Hobbies", description: "Beer-crafting, knitting, gardening, retrogaming " },
+  { name: "Experiences", description: "Trips, bungee-jumping, etc." },
+  { name: "Transport" }
+]
+
+if !Rails.env.production?
+  ActiveRecord::Base.connection.tables.each do |table|
+    if !table.in? ["schema_migrations", "ar_internal_metadata"]
+      puts "Truncating #{table}."
+      ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table} RESTART IDENTITY CASCADE;")
+    end
+  end
+end
+
+DEFAULT_CATEGORIES.each do |category|
+  Category.create(category.merge(default: true))
+  puts "Created category #{category[:name]}."
+end
+
+user = User.create(email: "user@example.com", password: "password")
+puts "Created user #{user.email}."
+
+AMOUNT_OF_EXPENSES = 200
+AMOUNT_OF_EXPENSES.times do
+  Expense.create(user: user, category_id: rand(DEFAULT_CATEGORIES.size + 1), amount: rand(1000) / 10.0)
+end
+puts "Created #{AMOUNT_OF_EXPENSES} random expenses."
